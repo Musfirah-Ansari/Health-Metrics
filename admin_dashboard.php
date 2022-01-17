5143d52c1245ecacfd74f706699898cc208aa184
Musfirah Fayyaz <musfirahfayyaz311@gmail.com>
2022-01-17 17:26:55 +0500

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,6 +1,4 @@
 <?php
-include('connections.php');
-include('header.php');
 error_reporting(0);
 ?>
 <!DOCTYPE html>
@@ -9,11 +7,19 @@ error_reporting(0);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
+<div class="back-dash">    
     <center>
         <h1>Admin Dashboard</h1>
     </center>
+    <?php
+include("header.php");
+include('connections.php');
+?>
+    </div>
 </body>
 </html>
