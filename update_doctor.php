48c808f2bc136487471279bd09e136c7f66b2cd6
Musfirah Fayyaz <musfirahfayyaz311@gmail.com>
2022-01-17 18:31:40 +0500

    Update update_doctor.php

--- update_doctor.php
+++ update_doctor.php
@@ -1,5 +1,5 @@
 <?php include('connections.php');
-include('header.php');
+
 error_reporting(0); ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,31 +7,88 @@ error_reporting(0); ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="form.css">
+    <title>Health Metrics | Update Doctor</title>
 </head>
 <body>
-    <center>
-        <h1>Update Doctor</h1>
-    </center>
-    <?php 
+    
+   
+    <div class="container">
+    <div class="title"> Update Doctor</div>
+    <div class="content">
+    
+     <form action="" method="post">
+        <div class="user-details">
+        <div class="input-box">
+            <span class="details">Doctor ID</span>
+            <input type="hidden" name="do_id"  value=" <?php echo $data['DOCID'] ?>"  />
+          </div>
+         
+        
+    <div class="input-box">
+            <span class="details">First Name </span>
+            <input type="text" name="fname" value=" <?php echo $data['DOC_FNAME'] ?>"  /> e.g. Muhammad
+          </div>
+          <div class="input-box">
+            <span class="details">Last Name </span>
+            <input type="text" name="lname" value="<?php echo $data['DOC_LNAME'] ?>"/> e.g. Jan
+          </div>
+        
+          </div>
+        <div class="gender-details">
+          <input type="radio" name="gender" id="dot-1" <?php if($data['GENDER'] == "Male"){echo "checked"; $dgender="Male";}?> value="Male" />
+          <input type="radio" name="gender" id="dot-2" <?php if($data['GENDER'] == "Female"){echo "checked"; $dgender="Female";}?> value="Female"/>
+          
+          <span class="gender-title">Gender</span>
+          <div class="category">
+            <label for="dot-1">
+            <span class="dot one"></span>
+            <span class="gender">Male</span>
+          </label>
+          <label for="dot-2">
+            <span class="dot two"></span>
+            <span class="gender">Female</span>
+          </label>
+         
+          </div>
+        </div>
+        <div class="input-box">
+            <span class="details">Email</span>
+            <input type="email" name="email" value=" <?php echo $data['DOC_EMAIL'] ?>" /> e.g. abc_123@xyz.com
+          </div>
+          <div class="input-box">
+            <span class="details">Qualification</span>
+            <input type="text" name="specialization" value=" <?php echo $data['SPECIALIZATION'] ?>" />e.g. Neurologist
+          </div>
+          <div class="input-box">
+            <span class="details">CNIC Number</span>
+            <input type="number" name="contact" value=" <?php echo $data['DOC_CONTACT'] ?>" />e.g. 03XXXXXXXXX
+          </div>
+        <div class="input-box">
+            <span class="details">CNIC Number</span>
+            <input type="number" name="cnic" value=" <?php echo $data['DOC_CNIC'] ?>" /> XXXXXXXXXXXXX
+          </div>
+          <div class="input-box">
+            <span class="details">Date Of Birth</span>
+            <input type="text" placeholder="YYYY-MM-DD" name="dob"  value="<?php echo $data['DOB'] ?>"> e.g. 2001-02-25 
+          </div>
+         
+        <div class="button">
+        <input type="submit" value="Submit" name="submit" style="margin: -4px">>
+          <input type="button" value="Go back!" onclick="history.back()">
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <?php 
     $doid=$_GET['id'];
     $sql_select="select * from doctor where DOCID={$doid}";
     $result_select=mysqli_query($conn,$sql_select);
     if(mysqli_num_rows($result_select)>0){
         while($data=mysqli_fetch_assoc($result_select)){
     ?>
-<form action="" method="post">
-        <input type="hidden" name="do_id" value="<?php echo $data['DOCID'] ?>"/><br>
-        First Name <input type="text" name="fname" value="<?php echo $data['DOC_FNAME'] ?>" /> e.g. Muhammad <br>
-        Last Name <input type="text" name="lname" value="<?php echo $data['DOC_LNAME'] ?>"/> e.g. Jan <br>
-        Gender:   Male <input type="radio" name="gender" <?php if($data['GENDER'] == "Male"){echo "checked"; $egender="Male";}?> value="Male" /> Female <input type="radio" name="gender" <?php if($data['GENDER'] == "Female"){echo "checked"; $egender="Female";}?> value="Female" /> <br>
-        Email <input type="email" name="email" value="<?php echo $data['DOC_EMAIL'] ?>"/> e.g. abc_123@xyz.com <br>
-        Qualification <input type="text" name="specialization" value="<?php echo $data['SPECIALIZATION'] ?>" /> e.g. Director <br>
-        Contact <input type="number" name="contact" value="<?php echo $data['DOC_CONTACT'] ?>" /> e.g. 03XXXXXXXXX <br>
-        CNIC <input type="number" name="cnic" value="<?php echo $data['DOC_CNIC'] ?>" /> XXXXXXXXXXXXX <br>
-        DOB <input type=text placeholder="YYYY-MM-DD" name="dob" value="<?php echo $data['DOB'] ?>"/> e.g. 2001-02-25 <br>
-        <input type="submit" name="submit" value="Update"/>
-    </form>
+      
     <?php }
     }
     $dofname = $_POST['fname'];
@@ -55,10 +112,7 @@ error_reporting(0); ?>
     else{
         echo 'Data not Updated :(';
     }
-    }
-    else{
-        echo 'All Fields are required!';
-    }
-    ?>
+  }
+   ?>
 </body>
 </html>
